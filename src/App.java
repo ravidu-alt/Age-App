b13ac7dcbc0179c7d1ffead6ce7cf541ecdbcc50
ravidu-alt <amk1001933@student.hamk.fi>
2024-09-07 11:37:48 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,57 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age = 100;
+        if (age == 18) {
+           System.out.println("You can drive a car."); 
+        }
+        if (age == 10|| age == 20|| age == 30|| age == 40|| age == 50|| age == 60|| age == 70|| age == 80 || age == 90|| age == 100|| age == 110|| age == 120) {
+            System.out.println("Anniversary Party.");
+        }
+        if (age==100) {
+            System.out.println("Congratulations.");
+            System.out.println("Congratulations.");
+            System.out.println("Congratulations.");
+         }
+         if (age >= 40 && age <= 50) {
+            System.out.println("happy-mid-life");
+         }
+        }
+
+        }
+
+
+        // if (age > 0 && age < 18) {
+        //    System.out.println("You are an underage."); 
+        // }
+        // if (age >= 15) {
+        //    System.out.println("You can drive a moped."); 
+        // }
+        // else if (age >= 65) {
+        //     System.out.println("You are retired.");
+        // }
+        // else{
+        //     System.out.println("You are an adult");
+        // }
+        
+    
+        // //if (age>0 && age<18) {
+        //     System.out.println("You are an underage.");
+        // }
+        // else if (age>=65) {
+        //     System.out.println("You are retired.");
+        // }
+        // else
+        // {
+        //     System.out.println("You are an adult.");
+        // }
+
+        //if (age>0 && age<18) {
+            //System.out.println("You are underage.");
+        //}
+        //else{
+         //   System.out.println("You are an adult.");
+        //}
+        //System.out.println("Press space to exit.");
+
+    
+
